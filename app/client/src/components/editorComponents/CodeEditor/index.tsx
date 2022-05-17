1fdb44955874e354c42523d635dea9708864afca
Rimil Dey <rimil@appsmith.com>
2022-05-17 15:45:42 +0530

    Code review changes

--- app/client/src/components/editorComponents/CodeEditor/index.tsx
+++ app/client/src/components/editorComponents/CodeEditor/index.tsx
@@ -275,7 +275,7 @@ class CodeEditor extends Component<Props, State> {
       }
 
       const isReadOnly = !this.props.input.onChange || this.props.disabled;
-      if (this.props.showLineNumbers && !isReadOnly) {
+      if (!isReadOnly) {
         const autoIndentKey = getAutoIndentShortcutKey();
         options.extraKeys[autoIndentKey] = (editor) => {
           autoIndentCode(editor);
